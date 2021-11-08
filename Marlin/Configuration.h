ab1fc1bdc995ed4e16d752ab9169c2c65f6fafe3
Dimetri Chau <32462212+Deonixlive@users.noreply.github.com>
2021-11-08 16:07:13 +0100

    iNIT
    INIT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1857,13 +1857,20 @@
 #define PREHEAT_1_TEMP_HOTEND 200
 #define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_TEMP_CHAMBER 0
-#define PREHEAT_1_FAN_SPEED     255 // Value from 0 to 255
+#define PREHEAT_1_FAN_SPEED     120 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED    100
 #define PREHEAT_2_TEMP_CHAMBER 35
-#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_2_FAN_SPEED   120 // Value from 0 to 255
+
+#define PREHEAT_1_LABEL       "PETG"
+#define PREHEAT_1_TEMP_HOTEND 230
+#define PREHEAT_1_TEMP_BED     80
+#define PREHEAT_1_TEMP_CHAMBER 0
+#define PREHEAT_1_FAN_SPEED     120 // Value from 0 to 255
+
 
 /**
  * Nozzle Park
